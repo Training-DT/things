3f96b1dd6fbbec85469875656c672513d720101b
thanchatip <thanchatip.coe@gmail.com>
2020-02-14 10:35:38 +0700

    :zap: add event to firebase database

--- eventForm.vue
+++ eventForm.vue
@@ -1,32 +1,31 @@
  <template>
     <div id="app" class="container">
         <div class="page-header">
-            <h1>Vue.js & Firebase <small>Fetch info</small></h1>
+            <h1> Add Event Feature </h1>
         </div>
-        <h5>Idol</h5>
+        <h5> Idol </h5>
         <select v-model="selectedIdol" id="idol" v-on:click="loadIdol()" >
             <option value="null">Please Select Idol</option>
-            <option  v-for="(idol) in idols" v-bind:key="idol.id" value:key="idol.name">{{idol.name}}</option>
+            <option  v-for="(idol) in idols" v-bind:key="idol.id" value:key="idol.name"> {{idol.name}} </option>
         </select>
         <br><br>
         <h5>Location</h5>
         <select v-model="selectedLocation" id="location" v-on:click="loadLocation()" >
-            <option value="null">Please Select Location</option>
-            <option  v-for="(location) in locations" v-bind:key="location.id" value:key="location.locationName">{{location.locationName}}</option>
-        </select>
-        <br><br>
-        <label for="startDate"><h5>Start Date</h5></label><br>
-        <input type="date" id="startDate" v-model="startDate">
-        <br><br>
+            <option value="null"> Please Select Location </option>
+            <option  v-for="(location) in locations" v-bind:key="location.id" value:key="location.locationName">
+                {{location.locationName}}
+            </option>
+        </select><br><br>
+        <label for="startDate"><h5> Start Date </h5></label><br>
+        <input type="date" id="startDate" v-model="startDate"><br><br>
         <h5>End Date</h5>
-        <input type="date" id="endDate" v-model="endDate">
-        <br><br>
-        <button type="button" class="btn btn-info" v-on:click="loadTodo()">  Add Event </button><br><br>
-        <h3>Result</h3>
-        <div class="mt-2">Idol: <strong>{{ selectedIdol }}</strong></div>         
-        <div class="mt-2">Location: <strong>{{ selectedLocation }}</strong></div>
-        <div class="mt-2">Start: <strong>{{ startDate }}</strong></div>
-        <div class="mt-2">End: <strong>{{ endDate }}</strong></div>
+        <input type="date" id="endDate" v-model="endDate"><br><br>
+        <button type="button" class="btn btn-info" v-on:click="addEvent()"> Add Event </button><br><br>
+        <h3> Result </h3>
+        <div class="mt-2"> Idol: <strong>{{ selectedIdol }}</strong></div>         
+        <div class="mt-2"> Location: <strong>{{ selectedLocation }}</strong></div>
+        <div class="mt-2"> Start: <strong>{{ startDate }}</strong></div>
+        <div class="mt-2"> End: <strong>{{ endDate }}</strong></div>
         </div>
 </template>
 
@@ -62,20 +61,19 @@
                 selectedStartDate: null,
                 selectedEndDate: null,
                 startDate: null,
-                endStart: null
+                endDate: null
             }
         },
         firebase(){
             return{
                 idols:db.collection('Person'),
-                locations:db.collection('Locations')
-                
+                locations:db.collection('Locations'),
+                events:db.collection('Events')      
             }
         },
         methods:{
             loadIdol() {
                 let idolList = [];
-                console.log("Hello");
                 db.collection('Person').get().then(function(querySnapshot) {
                     querySnapshot.forEach(function(doc) {  
                     let idol = {
@@ -89,10 +87,8 @@
             },
             loadLocation() {
                 let locationList = [];
-                console.log("Hello");
                 db.collection('Locations').get().then(function(querySnapshot) {
                     querySnapshot.forEach(function(doc) {  
-                        console.log(doc.id)
                     let location = {
                         id: doc.id,
                         locationName: doc.data().locationName,
@@ -102,10 +98,14 @@
                 });
                 this.locations = locationList;
             },
-            selectLocation(collectionID) {
-                
-            }
-                    
+            addEvent() {
+                db.collection('Events').add({
+                    idol: this.selectedIdol,
+                    location: this.selectedLocation,
+                    start: this.startDate,
+                    end: this.endDate,
+                });
+            }        
         }
     }
 </script>
